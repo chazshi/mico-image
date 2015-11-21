2ad6fa836843ddd1f731799d2cc49ab22a21a6fb
chazshi <sczhi55@gmail.com>
2015-11-21 16:52:19 +0800

    mico-image

---
+++ MiCO_image/script/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rocke on 15-7-21.
+ */
+var appId='rkb3roqqvevl2rctavxz8p94sfqwdne9s5gudiyhsicxovuh';
+var appKey='rtige83c3quf57nwd8786o62txm7jdwsavluv77rrcvjbljs';
+
+//APP_ID
+var APP_ID = "46354dd7-d9f7-4166-be3a-511ddb703695";
+//APP_NAME
+var APP_NAME = "MiCOAPP";
